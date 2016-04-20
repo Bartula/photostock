7effb2a0950545bafeba1a8730b647aacb597e52
bartosz.paszkowski <bartula87@gmail.com>
2016-04-20 16:22:09 +0200

    change name of methody and added return

--- src/pl/com/bottega/commons/math/probility/Probability.java
+++ src/pl/com/bottega/commons/math/probility/Probability.java
@@ -4,23 +4,13 @@ package pl.com.bottega.commons.math.probility;
  * Created by bartosz.paszkowski on 16.04.2016.
  */
 public class Probability {
-    public static Probability fromFraction(double value) {
-        if (value < 0 || value > 1)
-            throw new IllegalArgumentException("value must be <0,1>");
-        return new Probability(value);
-    }
-
-    public static Probability fromPercentage(double value) {
-        if (value < 0 || value > 100)
-            throw new IllegalArgumentException("value must be <0,100>");
-        return new Probability(value/100);
-    }
-/*
+    /*
     public enum ProbabilityRepresentation{
         PERC, FRAC;
     }
 */
     private double value;//wartość ułamkowa
+    public static final double DELTA = 0.00001;
 
 
     private Probability(double value){
@@ -35,4 +25,33 @@ public class Probability {
     public String toString(){
         return Double.toString(value);
     }
+
+    public static Probability fromFraction(double value) {
+        if (value < 0 || value > 1)
+            throw new IllegalArgumentException("value must be <0,1>");
+        return new Probability(value);
+    }
+
+    public static Probability fromPercentage(double value) {
+        if (value < 0 || value > 100)
+            throw new IllegalArgumentException("value must be <0,100>");
+        return new Probability(value/100);
+    }
+
+    @Override
+    public boolean equals(Object p) {
+        if (this == p) return true;
+        if (p == null || getClass() != p.getClass()) return false;
+
+        Probability that = (Probability) p;
+
+        return Double.compare(that.value, value) == 0;
+
+    }
+
+    /*@Override
+    public int hashCode() {
+        long temp = Double.doubleToLongBits(value);
+        return (int) (temp ^ (temp >>> 32));
+    }*/
 }
